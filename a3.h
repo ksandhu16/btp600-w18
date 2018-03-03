c63dc49f8decf2c0b408d9205df0906667b0155f
cathyatseneca <cathyatseneca@gmail.com>
2018-03-03 15:27:06 -0500

    a3.h

--- a3.h
+++ a3.h
@@ -1,16 +1,32 @@
 class Builder{
 
+public: 
+
 };
 
 class Director{
+	//add what data members you think you need here
+
+public:
+	void setBuilder(Builder& b){
+		//...
+	}
+
+	Room* buildRoom(int numStudents){
+		Room* rc=new Room();
+		//...
+		return rc;
+	}
 
 };
 class ALCBuilder{
+public:
 
 };
 class LabBuilder{
+public:
 
 };
 class StepRoomBuilder{
-
+public:
 };
